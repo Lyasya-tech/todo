62ae98ebce38769265dce84e3403fc300fc0a4a0
Lyasya-tech <111197907+Lyasya-tech@users.noreply.github.com>
2023-11-18 14:30:39 +0100

    feat: getUserName added

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Assignment } from '../models/assignment.model'; 
 import { AssignmentService } from './assignment.service'; 
+import { User } from '../models/user.model';
+import { Task } from '../models/task.model';
+import { UserService } from '../users/user.service';
 
 @Component({
     selector: 'app-assignments', 
@@ -16,12 +19,16 @@ export class AssignmentsComponent implements OnInit {
   assignment: Assignment={id:0,  
                           userId: 0,
                           taskId: 0 ,
-                          timestamp: new Date().toISOString()} 
+                          timestamp: new Date().toISOString()}; 
+  users: User[] = [];
+  task: Task;
+
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private assignmentsService: AssignmentService
+    private assignmentsService: AssignmentService,
+    private userService: UserService
   ) 
   {
     console.log('constructor initiated');
@@ -32,10 +39,18 @@ export class AssignmentsComponent implements OnInit {
     this.assignmentsService.getAssignmentListUpdates().subscribe(updatedAssignments => { 
       this.assignments = updatedAssignments; 
     });
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
   }
 
   createNewAssignment() {
     this.assignmentsService.createAssignment(this.assignment).subscribe(() => {}); 
+  }
+
+  getUserName(userId: number){
+    const user = this.users.find(user => user.id == userId);
+    return user ? user.name : 'Unknown User';
   }
 }
 

491a35aca415c2e30d0e89fe84ec88e1bd711552
Nada2911 <s51992@edu.campus02.at>
2023-11-27 21:04:49 +0100

    assignment edit added(half finished)

--- src/app/assignments/assignment-edit/assignment-edit.component.ts
+++ src/app/assignments/assignment-edit/assignment-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Assignment } from 'src/app/models/assignment.model';
+import { AssignmentService } from '../assignment.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-assignment-edit',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment-edit.component.css']
 })
 export class AssignmentEditComponent {
+    assignment: Assignment = {
+    id: 0,
+    userId: 0,
+    taskId: 0,
+    timestamp: ''
+};
+
+  editMode: string;
+  assignmentService: any;
+  constructor(private route: ActivatedRoute, private taskService: AssignmentService) {
+    this.editMode = this.route.snapshot.data.editMode;
+  }
+  ngOnInit() {
+    if (this.editMode !== 'new'){
+      this.route.parent.params.subscribe(params => {
+        const assignmentId = +params['id']; 
+        this.assignmentService.getAssignmentById(assignmentId).subscribe({
+          next: (assignment: Assignment) => {
+          this.assignment = assignment;  
+        }, error: errMessage => {
+          console.error('no assignment was fetched', errMessage);
+        }
+      });
+      });
+    }  
+  }
+
+  onFormSubmit() {
+    if (this.editMode === 'new') {
+        // Create a new assignment
+        this.assignmentService.createAssignment(this.assignment).subscribe(newAssignment => {
+            // Handle the new assignment as needed (e.g., display success message)
+        });
+    } else {
+        // Update an existing assignment
+        this.assignmentService.updateAssignment(this.assignment).subscribe(updatedAssignment => {
+            // Handle the updated assignment as needed (e.g., display success message)
+        });
+    }
+}
 
 }
